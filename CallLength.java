c0ce58e9e99e93c002be7f1a002e13f473bb1b6c
SmitaDugur <smitadugur25@gmail.com>
2021-12-14 19:45:15 +0530

    ADDED:CallLength

---
+++ CallLength.java
@@ -0,0 +1,13 @@
+public class CallLength {
+
+        public static void main(String[] args) {
+                int x1, x2, y1, y2;
+                double answer;
+                x1=3;
+                x2=6;
+                y1=9;
+                y2=4;
+                answer = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+                System.out.println(answer);
+        }
+}
